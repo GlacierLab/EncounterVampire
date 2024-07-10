c22fa557c9d39e0e5329ee1444d0fd955204ca46
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2024-07-10 17:25:39 +1000

    Framerate Unlock

---
+++ BepInExFrameratePlugin/Vampire2Framerate/Breach.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace Vampire2Framerate
+{
+    internal class Breach
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //FPS Breach
+        [HarmonyPatch(typeof(QualityCtrl), "SetFPS")]
+        [HarmonyPrefix]
+        static void SetFPS(int fpsIn)
+        {
+            Plugin.Log.LogInfo("FPS Breach Hits!");
+            UnityEngine.QualitySettings.vSyncCount = 1;
+            UnityEngine.Application.targetFrameRate = -1;
+        }
+
+        //Skip Loading
+        [HarmonyPatch(typeof(LoadingBuffer), "Start")]
+        [HarmonyPrefix]
+        static void Start()
+        {
+            Plugin.Log.LogInfo("Skip Logo!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("opentheme");
+        }
+
+        //Fix Fullscreen switch
+        [HarmonyPatch(typeof(LoadingInit), "Start")]
+        [HarmonyPrefix]
+        static void Start1()
+        {
+            Plugin.Log.LogInfo("No Fullscreen Switch!");
+        }
+
+    }
+}
---
+++ BepInExFrameratePlugin/Vampire2Framerate/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace Vampire2Framerate;
+
+[BepInPlugin("qinlili.vampiresmelody2.fpsbreach", "Unlock FPS", "1.0.0.0")]
+public class Plugin : BasePlugin
+{
+    public static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin FPS Breach is loaded!");
+        Log.LogInfo("Processing...");
+        Harmony.CreateAndPatchAll(typeof(Breach));
+        Log.LogInfo("Patched!");
+    }
+}
